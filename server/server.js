16ec7ba009985062a33401a46c978583d69a12df
Myron <dubtwee@gmail.com>
2019-11-20 13:48:33 -0600

    adding quotes endpoint

--- server/server.js
+++ server/server.js
@@ -1,9 +1,15 @@
 let express = require('express');
+const quotes = require('./modules/quotes');
 
 const PORT = 5000;
 let app = express();
 
-app.use(express.static('client'))
+app.use(express.static('client'));
+
+app.get('/quotes', (req, res) => {
+    console.log('GET Request for quotes.');
+    res.send(quotes.list);
+});
 
 app.listen(PORT, function(){
     console.log('listening on port', PORT);
