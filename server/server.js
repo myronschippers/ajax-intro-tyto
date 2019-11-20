8b6d794a031b189b32fcf47896917c70d651cdd6
Myron <dubtwee@gmail.com>
2019-11-20 13:20:00 -0600

    adding base homepage route

--- server/server.js
+++ server/server.js
@@ -1,6 +1,11 @@
 let express = require('express');
 let app = express();
 
+// Homepage Route
+app.get('/', (req, res) => {
+    res.send('Welcome to the homepage!!!');
+})
+
 app.listen(5000, function(){
     console.log('listening on port', 5000);
 });
