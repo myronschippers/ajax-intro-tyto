23395e1814378ef237a6f8dfa6894326fd678068
Myron <dubtwee@gmail.com>
2019-11-20 13:17:56 -0600

    adjusting starting codebase and adding some boilerplate code to kick things off

---
+++ server/modules/quotes.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const router = express.Router();
+
+let quotesData = [
+    { text: 'I\'m not going to school just for the academics - I wanted to share ideas, to be around people who are passionate about learning.', author: 'Emma Watson' },
+    { text: 'Remember there\'s no such thing as a small act of kindness. Every act creates a ripple with no logical end.', author: 'Scott Adams' },
+    { text: 'Intelligence plus character-that is the goal of true education.', author: 'Martin Luther King, Jr.' }
+];
+let index = 0;
+
+function getNextQuote() {
+	let result = quotesData[index];
+	index += 1; // Increase the index
+	index = index % quotesData.length; // Ensures we stay in bounds
+    return result;
+}
+
+module.exports = {
+    list: quotesData,
+    getNext: getNextQuote
+};
---
+++ server/server.js
@@ -0,0 +1,6 @@
+let express = require('express');
+let app = express();
+
+app.listen(5000, function(){
+    console.log('listening on port', 5000);
+});
